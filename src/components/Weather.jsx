93d0d8f6f271c9d3f6f6004873fe5401c60d2e8d
Theekshana Nirmal <theekshanab1234@gmail.com>
2025-04-13 20:40:28 +0530

    Add Weather component to display current weather information

---
+++ src/components/Weather.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Weather.css";
+import search_icon from "./../assets/search.png";
+import clear_icon from "./../assets/clear.png";
+import rain_icon from "./../assets/rain.png";
+import snow_icon from "./../assets/snow.png";
+import cloud_icon from "./../assets/cloud.png";
+import wind_icon from "./../assets/wind.png";
+import humidity_icon from "./../assets/humidity.png";
+import drizzle_icon from "./../assets/drizzle.png";
+
+const Weather = () => {
+  return (
+    <div className="weather">
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <img src={search_icon} alt="" />
+      </div>
+      <img src={clear_icon} alt="" className="weather-icon" />
+      <p className="temperature">25°C</p>
+      <p className="location">New York</p>
+      <div className="weather-data">
+        <div className="col">
+          <img src={humidity_icon} alt="" />
+          <div>
+            <p>75%</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src={wind_icon} alt="" />
+          <div>
+            <p>1.8 Km/h</p>
+            <span>Wind Speed</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
